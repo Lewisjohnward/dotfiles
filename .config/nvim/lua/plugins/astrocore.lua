0752af0e2db53e22d463344f56d72398719f2c55
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-10-21 15:10:41 +0100

    add hotkey to open in windsurf

--- .config/nvim/lua/plugins/astrocore.lua
+++ .config/nvim/lua/plugins/astrocore.lua
@@ -74,6 +74,8 @@ return {
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
         -- ["<Leader>b"] = { desc = "Buffers" },
+        ["<Leader>o"] = { desc = " Open With" },
+        ["<Leader>ow"] = { "<cmd>!windsurf %<CR>", desc = "Open in Windsurf" },
 
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
